cea767f2e7227e028ddacf285d03d6fb10a166aa
Mosinmiloluwa Owoso <mosinmiloluwa.o@gmail.com>
2019-03-27 17:28:40 +0100

    ft(inbox ui): fetch user messages for inbox [finishes #164911482]

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static(path.join(__dirname, './src')));
 
 app.get('/', (req, res) => res.status(200).sendFile(path.join(__dirname, './src/html/login.html')));
 
-const port = process.env.PORT || 6000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on ${port}`));
 
 export default app;
